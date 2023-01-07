28c8ea7a2259a074a6902dfb3aaaf59964cd631c
gjssss <gaojinsong2010@gmail.com>
2023-01-07 18:37:14 +0800

    实现垂直头像

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,19 @@
 <template>
-  <div>happy new year</div>
+  <div class="container">
+    <UserComment></UserComment>
+  </div>
 </template>
+<script setup>
+import UserComment from "@/components/UserComment.vue"
+</script>
+<style>
+* {
+  margin: 0;
+  padding: 0;
+}
 
+.container {
+  width: 80vw;
+  margin: auto;
+}
+</style>
