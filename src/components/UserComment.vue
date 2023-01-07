27a49c056dffe8258a2775a58d3488dd13bc1415
gjssss <1615590366@qq.com>
2023-01-07 18:37:14 +0800

    实现垂直头像

---
+++ src/components/UserComment.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="comment-container">
+        <CommentItem v-for="item in commData" :key="item.id" :data="item"></CommentItem>
+    </div>
+</template>
+<script setup>
+import CommentItem from "@/components/widgets/CommentItem.vue"
+const commData = [{
+    id: 1,
+    name: 'wff',
+    content: "喜悦伴着汗水，成功伴着艰辛，遗憾激励奋斗，我们不知不觉地走进了2023年，在新年来临之际，首先我代表信息中心祝各位新年快乐，万事如意!"
+}, {
+    id: 2,
+    name: 'gjs',
+    content: "喜悦伴着汗水，成功伴着艰辛，遗憾激励奋斗，我们不知不觉地走进了2023年，在新年来临之际，首先我代表信息中心祝各位新年快乐，万事如意!"
+}]
+</script>
+
+<style scoped>
+.comment-container {
+    width: 100%;
+    height: 600px;
+    border-radius: 10px;
+    background: linear-gradient(315deg, #42d392 25%, #647eff);
+    padding: 10px 5px;
+    box-shadow: 1px 2px 10px #267a54;
+}
+</style>
---
+++ src/components/widgets/CommentItem.vue
@@ -0,0 +1,50 @@
+<template>
+    <div class="comment-item">
+        <div class="header">
+            <img :src="'https://avatars.dicebear.com/api/micah/' + data.name + '.svg?mouth=smile&eyebrows=up'" alt="头像">
+        </div>
+        <div class="bubble" />
+        <div class="content">
+            <p>{{ data.content }}</p>
+        </div>
+    </div>
+</template>
+
+<script setup>
+
+defineProps(['data'])
+
+</script>
+
+<style scoped>
+.comment-item {
+    width: 90%;
+    margin: 10px 0;
+}
+
+.comment-item .header {
+    width: 60px;
+    height: 60px;
+    border-radius: 50%;
+    overflow: hidden;
+    border: 1px solid white;
+    background-color: #fff;
+}
+
+.comment-item .content {
+    background-color: #fff;
+    padding: 5px;
+    border-radius: 20px;
+}
+
+.comment-item .bubble {
+    position: relative;
+    border-top: 8px solid transparent;
+    border-right: 8px solid transparent;
+    border-bottom: 8px solid white;
+    border-left: 8px solid transparent;
+    width: 0;
+    height: 0;
+    left: 25px;
+}
+</style>
