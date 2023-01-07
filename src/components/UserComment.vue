bcb65dae6021431ca1cb1a0d398f6d60c0407c69
gjssss <gaojinsong2010@gmail.com>
2023-01-07 19:04:39 +0800

    实现水平头像

--- src/components/UserComment.vue
+++ src/components/UserComment.vue
@@ -12,17 +12,31 @@ const commData = [{
 }, {
     id: 2,
     name: 'gjs',
-    content: "喜悦伴着汗水，成功伴着艰辛，遗憾激励奋斗，我们不知不觉地走进了2023年，在新年来临之际，首先我代表信息中心祝各位新年快乐，万事如意!"
+    content: "短句子"
+}, {
+    id: 3,
+    name: 'ggg',
+    content: "超长句子超长句子超长句子超长句子超长句子超长句子超长句子超长句子超长句子超长句子超长句子超长句子超长句子超长句子超长句子超长句子超长句子超长句子超长句子超长句子超长句子超长句子超长句子超长句子超长句子超长句子超长句子超长句子超长句子超长句子超长句子超长句子超长句子超长句子"
+}, {
+    id: 4,
+    name: 'fff',
+    content: "超长句子超长句子超长句子超长句子超长句子超长句子超长句子超长句子超长句子超长句子超长句子超长句子超长句子超长句子超长句子超长句子超长句子超长句子超长句子超长句子超长句子超长句子超长句子超长句子超长句子超长句子超长句子超长句子超长句子超长句子超长句子超长句子超长句子超长句子"
 }]
 </script>
 
 <style scoped>
 .comment-container {
     width: 100%;
-    height: 600px;
+    height: 400px;
     border-radius: 10px;
     background: linear-gradient(315deg, #42d392 25%, #647eff);
     padding: 10px 5px;
     box-shadow: 1px 2px 10px #267a54;
+    margin-top: 20px;
+    overflow: scroll;
+    scrollbar-width: none;
+}
+.comment-container::-webkit-scrollbar{
+    display: none;
 }
 </style>
