8c275ce3b8d3f1a8b12983dee211511550ba493a
gjssss <gaojinsong2010@gmail.com>
2023-01-07 19:51:21 +0800

    整理代码

--- src/components/UserComment.vue
+++ src/components/UserComment.vue
@@ -1,27 +1,11 @@
 <template>
     <div class="comment-container">
-        <CommentItem v-for="item in commData" :key="item.id" :data="item"></CommentItem>
+        <CommentItem v-for="item in data" :key="item.id" :data-item="item"></CommentItem>
     </div>
 </template>
 <script setup>
 import CommentItem from "@/components/widgets/CommentItem.vue"
-const commData = [{
-    id: 1,
-    name: 'wff',
-    content: "喜悦伴着汗水，成功伴着艰辛，遗憾激励奋斗，我们不知不觉地走进了2023年，在新年来临之际，首先我代表信息中心祝各位新年快乐，万事如意!"
-}, {
-    id: 2,
-    name: 'gjs',
-    content: "短句子"
-}, {
-    id: 3,
-    name: 'ggg',
-    content: "超长句子超长句子超长句子超长句子超长句子超长句子超长句子超长句子超长句子超长句子超长句子超长句子超长句子超长句子超长句子超长句子超长句子超长句子超长句子超长句子超长句子超长句子超长句子超长句子超长句子超长句子超长句子超长句子超长句子超长句子超长句子超长句子超长句子超长句子"
-}, {
-    id: 4,
-    name: 'fff',
-    content: "超长句子超长句子超长句子超长句子超长句子超长句子超长句子超长句子超长句子超长句子超长句子超长句子超长句子超长句子超长句子超长句子超长句子超长句子超长句子超长句子超长句子超长句子超长句子超长句子超长句子超长句子超长句子超长句子超长句子超长句子超长句子超长句子超长句子超长句子"
-}]
+defineProps(['data'])
 </script>
 
 <style scoped>
